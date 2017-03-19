ee34edddd2b6c3bacc0d47743889cfa3d628b4f4
Ivon Liu <me@ivonliu.com>
2017-03-19 07:29:34 -0700

    you are a jerk

--- ivonKF.py
+++ ivonKF.py
@@ -9,35 +9,39 @@ class KF:
         self.dt = 1
         x0 = 0  #hopefully
         v0 = 0  #it should eventually fix itself
+        a0 = 0
 
-        self.estimate = np.array([[x0], [v0]]) # x
-        self.stateCovariance = np.array( [[0.5, 1e-9], [1e-9, 0.5]] ) # p
+        self.estimate = np.array([[x0], [v0], [a0]]) # x
+        self.stateCovariance = np.array( [[0.5, 1e-9, 1e-9], [1e-9, 0.5, 1e-9], [1e-9, 1e-9, 0.5]] ) # p
 
-        self.stateCovarianceNoise = np.array( [[0.1, 0], [0, 0.1]] ) # q
-        self.estimatePredictNoise = np.array( [[0], [0]] ) # w
-        self.dataNoise = np.array( [[0], [0]] ) # z
+        self.stateCovarianceNoise = np.array( [[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]] ) # q
+        self.estimatePredictNoise = np.array( [[0], [0], [0]] ) # w
+        self.dataNoise = np.array( [[0], [0], [0]] ) # z
 
-        self.dataError = np.array( [[0.5, 1e-9], [ 1e-9, 1]] ) # r
+        self.dataError = np.array( [[0.5, 1e-9, 1e-9], [ 1e-9, 1, 1e-9], [1e-9, 1e-9, 1.5]] ) # r
 
         # If self.dt is varying this needs to move the the beginning of the for loop
-        self.convertA = np.array( [[1, self.dt],[0, 1]] ) # A
-        self.convertB = np.array( [[self.dt**2/2],[self.dt]] ) # B
-        self.convertC = np.array( [[1, 0], [0, 1]] ) # C
-        self.convertH = np.array( [[1, 0], [0,1]] ) # H
+        self.convertA = np.array( [[1, self.dt, self.dt**2/2],[0, 1, self.dt], [0, 0, 1]] ) # A
+        self.convertB = np.array( [[self.dt**3/6],[self.dt**2/2], [self.dt]] ) # B
+        self.convertC = np.array( [[1, 0, 0], [0, 1, 0], [0, 0, 1]] ) # C
+        self.convertH = np.array( [[1, 0, 0], [0,1,0], [0, 0, 1]] ) # H
 
 
     def predict(self, past_points):
-        acceleration = np.array([[delta2(self.dt, past_points)]])
+        jerk = np.array([[delta3(self.dt, past_points)]])
 
         estimatePredict = np.dot( self.convertA, self.estimate ) + np.dot( self.convertB, acceleration ) + self.estimatePredictNoise
 
-        stateConvariancePredict = np.dot( self.convertA, np.dot( self.stateCovariance, self.convertA.T )) + self.stateCovarianceNoise
-        stateConvariancePredict[0][1] = 0
-        stateConvariancePredict[1][0] = 0
+        temp2 = np.dot( self.convertA, np.dot( self.stateCovariance, self.convertA.T )) + self.stateCovarianceNoise
+        stateConvariancePredict = [[0, 0, 0] for i in range(3)]
+        stateConvariancePredict[0][0] = temp2[0][0]
+        stateConvariancePredict[1][1] = temp2[1][1]
+        stateConvariancePredict[2][2] = temp2[2][2]
        
         x_cur = past_points[-1]
         v_cur = delta1(self.dt, past_points)
-        cur_state = np.array([[x_cur], [v_cur]])
+        a_cur = delta2(self.dt, past_points)
+        cur_state = np.array([[x_cur], [v_cur], [a_cur]])
         d = np.dot( self.convertC, cur_state ) + self.dataNoise
         
         print()
@@ -60,3 +64,5 @@ class KF:
 
 
         return futurePoints
+
+        

67c53a4c4f75961e6cc6658e2f61020a5fc0a696
Jason Peng <jasonpeng023@gmail.com>
2017-03-19 03:33:02 -0700

    update function

--- src/predict/Function.java
+++ src/predict/Function.java
@@ -4,13 +4,23 @@ import java.awt.Point;
 
 public class Function {
 	
-	
 	public static Pose function(double time){
-		Point p = new Point((int) (10*time),(int)(100*Math.sin(time/10)) );
-		double dy = 100*(Math.sin(time/10)-Math.sin((time-0.01)/10));
-		double dx = 10*(0.01);
+		double x = getX(time);
+		double y = getY(time);
+		double dx = x-getX(time-0.01);
+		double dy = y-getY(time-0.01);
 		
-		double o = -Math.atan2(dy,dx);
-		return new Pose(p, o);
+		Point p = new Point((int)x,(int)(y));
+		double o = Math.atan2(dy,dx);
+		
+		return new Pose(p, -o);
+	}
+	
+	private static double getY(double time){
+		return 100*Math.sin(time/10);
+	}
+	
+	private static double getX(double time){
+		return 10*time;
 	}
 }

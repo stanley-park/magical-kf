7e0887fb38e75f2ccb89d66c27757ca5e3b67720
Oscar <o.car.w2@gmail.com>
2017-03-18 21:48:25 -0700

    Fix method names and change edit

--- MatrixInterface.java
+++ MatrixInterface.java
@@ -3,24 +3,24 @@ public interface MatrixInterface {
 	// no boolean modify means function returns a new matrix
 
 	// matrix addition 
-	public Matrix m_add(Matrix m);
+	public Matrix add(Matrix m);
 
-	public Matrix m_add(Matrix m, boolean modify);
+	public Matrix add(Matrix m, boolean modify);
 
 
 	// matrix multiplication 
-	public Matrix m_multiply(Matrix m);
+	public Matrix multiply(Matrix m);
 
-	public Matrix m_multiply(Matrix m, boolean modify);
+	public Matrix multiply(Matrix m, boolean modify);
 
 
 	// transpose matrix 
-	public Matrix m_transpose(Matrix m);
+	public Matrix transpose(Matrix m);
 
-	public Matrix m_transpose(Matrix m, boolean modify);
+	public Matrix transpose(Matrix m, boolean modify);
 
 
 	// change element in matrix
-	public void m_edit(double[][] a1);
+	public double[][] edit();
 
 }
